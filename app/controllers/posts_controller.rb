af0c9072fd1884718a903a3f082d2779206ae39e
Fumiya Kai <fumiya.kai@kellogg.northwestern.edu>
2023-06-05 03:19:13 +0000

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,25 @@
 class PostsController < ApplicationController
 
+
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.uploaded_image.attach(params["post"]["uploaded_image"])
-    @post["user_id"] = @current_user["id"]
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+    if @current_user
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["place_id"] = params["post"]["place_id"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
+      @post["user_id"] = @current_user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+      redirect_to "/places/#{@post["place_id"]}"
   end
 
 end
